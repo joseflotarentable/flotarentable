3873b207f0187b580cc08959fd3234cedf7a54ac
joseflotarentable <7h6fg5p84d@privaterelay.appleid.com>
2026-06-11 17:19:01 +0200

    Usar supabase-js como dependencia local en vez de CDN esm.sh

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,4 +1,4 @@
-import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+import { createClient } from "@supabase/supabase-js";
 
 const SUPABASE_URL = "https://ktfrzckzxnqsqfvglwfh.supabase.co";
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt0ZnJ6Y2t6eG5xc3Fmdmdsd2ZoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU4MzY0MTQsImV4cCI6MjA5MTQxMjQxNH0.3O4ZZ53Ww6s5hLXiaPBFlpswTkYWKSS5BRMTjWt-A34";
